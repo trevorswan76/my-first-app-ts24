4f87ede52754f90f0fe48f8db2832289802ced35
trevorswan76 <tls118@georgetown.edu>
2024-11-17 23:01:17 -0500

    Update stocks.py

--- app/stocks.py
+++ app/stocks.py
@@ -2,16 +2,11 @@
 
 # LOCAL DEV (ENV VARS)
 
-import os
-
-from dotenv import load_dotenv
 from pandas import read_csv
 from plotly.express import line
 
 
-load_dotenv() # looks in the ".env" file for env vars
-
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
+from app.alpha_service import API_KEY
 
 
 def fetch_stocks_csv(symbol):
